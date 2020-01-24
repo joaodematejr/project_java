32ea5647ad1b91e3cb4bd88658a8184477279700
joaodematejr <joaodematejr@gmail.com>
2020-01-23 21:54:19 -0300

    Correção do CORS da aplicação

--- app/src/main/java/com/joaodematejr/api/controllers/PeopleController.java
+++ app/src/main/java/com/joaodematejr/api/controllers/PeopleController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,28 +23,29 @@ import com.joaodematejr.api.documents.People;
 import com.joaodematejr.api.responses.Response;
 import com.joaodematejr.api.services.PeopleService;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping(path = "/api/people")
 public class PeopleController {
-	
+
 	@Autowired
 	private PeopleService peopleService;
-	
-	//LISTAR TODOS
+
+	// LISTAR TODOS
 	@GetMapping
-	public ResponseEntity<Response<List<People>>> listAll(){
+	public ResponseEntity<Response<List<People>>> listAll() {
 		return ResponseEntity.ok(new Response<List<People>>(this.peopleService.listAll()));
 	}
-	
-	//LISTAR POR ID
+
+	// LISTAR POR ID
 	@GetMapping(path = "/{id}")
-	public ResponseEntity<Response<People>> listById(@PathVariable(name = "id") String id){
+	public ResponseEntity<Response<People>> listById(@PathVariable(name = "id") String id) {
 		return ResponseEntity.ok(new Response<People>(this.peopleService.listById(id)));
 	}
-	
-	//CADASTRAR
+
+	// CADASTRAR
 	@PostMapping
-	public ResponseEntity<Response<People>> register(@Valid @RequestBody People people, BindingResult result){
+	public ResponseEntity<Response<People>> register(@Valid @RequestBody People people, BindingResult result) {
 		if (result.hasErrors()) {
 			List<String> errs = new ArrayList<String>();
 			result.getAllErrors().forEach(err -> errs.add(err.getDefaultMessage()));
@@ -51,33 +53,31 @@ public class PeopleController {
 		}
 		people.setRegistrationDate(Calendar.getInstance());
 		return ResponseEntity.ok(new Response<People>(this.peopleService.register(people)));
-		
+
 	}
-	
-	//ATUALIZAR
+
+	// ATUALIZAR
 	@PutMapping(path = "/{id}")
-	public ResponseEntity<Response<People>> update(@PathVariable(name = "id") String id, @Valid @RequestBody People people, BindingResult result){
+	public ResponseEntity<Response<People>> update(@PathVariable(name = "id") String id,
+			@Valid @RequestBody People people, BindingResult result) {
 		if (result.hasErrors()) {
 			List<String> errs = new ArrayList<String>();
 			result.getAllErrors().forEach(err -> errs.add(err.getDefaultMessage()));
 			return ResponseEntity.badRequest().body(new Response<People>(errs));
 		}
 		people.setId(id);
-		
+
 		people.setUpdateData(Calendar.getInstance());
 		people.setRegistrationDate(people.getRegistrationDate());
 		return ResponseEntity.ok(new Response<People>(this.peopleService.update(people)));
-		
+
 	}
-	
-	//REMOVER
+
+	// REMOVER
 	@DeleteMapping(path = "/{id}")
-	public ResponseEntity<Response<Integer>> remove(@PathVariable(name = "id") String id){
+	public ResponseEntity<Response<Integer>> remove(@PathVariable(name = "id") String id) {
 		this.peopleService.remove(id);
 		return ResponseEntity.ok(new Response<Integer>(1));
 	}
-	
-	
-	
-	
+
 }
